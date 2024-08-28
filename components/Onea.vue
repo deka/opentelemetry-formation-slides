d4bd13c1147f18efd2b31aba66d7c56a392f3f5a
deka <deka@mail.com>
2024-08-28 18:07:15 +0200

    Add configuration files and ignore patterns

---
+++ components/Onea.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex items-center cursor-pointer" @click="goto">
+        <img src="/images/logo-cercle.png" class="w-6" />
+        <span class="ml-2 text-[#3C9B96] font-bold text-2xl">ONEA</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+const goto = () => {
+    window.open('https://www.linkedin.com/in/dimitri-rayer/', '_blank');
+}
+</script>
+
+<style scoped>
+
+</style>
